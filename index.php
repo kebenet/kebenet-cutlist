c27949352016b91f14c0fed23f7d007da27d0121
renova <146024094+kebenet@users.noreply.github.com>
2025-05-27 03:47:41 +0800

    rename template-db index.php

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
     <script src="src/crud.js"></script>
-    <script src="src/templatedb.js"></script>
+    <script src="src/template-db.js"></script>
     <style>
         body {
             font-family: 'Inter', sans-serif;
